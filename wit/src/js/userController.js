a7451deabea95868b3f0e07ae317b5415c261d65
GamjaJeon <indian_potato@naver.com>
2024-02-02 17:06:36 +0900

    style: login quizhome 페이지

---
+++ wit/src/js/userController.js
@@ -0,0 +1,18 @@
+function getUserNickname() {
+  const loginInput = document.getElementById('login-input')
+  const player = {
+    name: loginInput.value,
+    subject: '',
+    startTime: '',
+  }
+
+  console.log(loginInput.value)
+  localStorage.setItem('player', JSON.stringify(player))
+  console.log('Before Redirect')
+  location.href = 'quiz_home.html'
+  console.log('After Redirect')
+}
+
+function createQuizBtn() {
+  const quizs = []
+}
